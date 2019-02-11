543ce592ff2005f5f45122ebe8fd79ac37833432
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-02-11 16:02:38 -0500

    Footer added

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -3,13 +3,17 @@ import './Header.css';
 import ImageThumbnail from './ImageThumbnail';
 import HeaderTitle from './HeaderTitle';
 import HeaderContent from './HeaderContent';
+import Footer from './Footer';
 
 function HeaderContainer(){
     return(
         <div className = 'header-conatainer'>
-            <ImageThumbnail />
+            <ImageThumbnail />    
             <HeaderTitle />
             <HeaderContent /> 
+            <Footer />
+            
+            
         </div>
-    )
+    );
 }
