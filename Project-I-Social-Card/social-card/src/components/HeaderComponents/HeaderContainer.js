e80a519db3dc063f2b78881468cbe4583d7b89f6
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-02-11 15:38:26 -0500

    HeaderContent.js is updated and Header Container importing files

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+
+function HeaderContainer(){
+    return(
+        <div className = 'header-conatainer'>
+            <ImageThumbnail />
+            <HeaderTitle />
+            <HeaderContent /> 
+        </div>
+    )
+}
